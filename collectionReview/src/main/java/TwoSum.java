3f3533e2293d811b81469d23fa3bb1ac2fb579b6
Abdellatif Zouliga <zuuliga@gmail.com>
2023-08-02 09:58:21 -0400

    added some comment

--- collectionReview/src/main/java/TwoSum.java
+++ collectionReview/src/main/java/TwoSum.java
@@ -22,6 +22,8 @@ public class TwoSum {
 
     public static int[] toSumBruteForce(int[] array, int target) {
 
+        // time complexity: n*n => O(n^2)
+        // space complexity: O(n)
         for (int i = 0; i < array.length; i++) {
             for (int j = i; j < array.length; j++) {
                 if (array[i] + array[j] == target) {
@@ -34,21 +36,20 @@ public class TwoSum {
 
     public static int[] twoSumMap(int[] array, int target){
 
+        // time complexity: n+n with two loops or n with one loop. result => O(n)
+        // space complexity: O(n)
         Map<Integer, Integer> map = new LinkedHashMap<>();
-//
-        // adding all element in the array and their indexes to map
-//        for (int i = 0; i < array.length; i++) {
-//            map.put(array[i], i);
-//        }
+
+        //  adding all element in the array and their indexes to map
+        //  for (int i = 0; i < array.length; i++) {
+        //     map.put(array[i], i);
+        //  }
 
         for (int i = 0; i < array.length; i++) {
-
             map.put(array[i], i);  // we can use one separate loop to add all element and their indexes to map
-
             if (map.containsKey(target - array[i])){
                 return new int[] {map.get(target - array[i]), i};
             }
-
         }
         return array;
     }
