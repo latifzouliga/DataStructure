fdee5db433b06522eb6eaf7aaee0cdbee189d4fb
Abdellatif Zouliga <zuuliga@gmail.com>
2023-08-26 09:54:40 -0400

    some tasks addedd.

--- ds_08_Trees/src/main/java/MyTree.java
+++ ds_08_Trees/src/main/java/MyTree.java
@@ -1,8 +1,6 @@
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
-public class MyTree{
+public class MyTree {
 
     TNode root;
 
@@ -39,19 +37,20 @@ public class MyTree{
     }
 
 
+    //==================================== Depth first ===============================
     // PreOrder Traversal of the tree
     // Root -> Left -> Right
     public void preOrderTraversal(TNode root) {
 
         if (root == null) return;
-        System.out.println(root.value );
+        System.out.println(root.value);
         preOrderTraversal(root.leftChild);
         preOrderTraversal(root.rightChild);
     }
 
     // In-Order Traversal
     // Left -> Root -> Right
-    public void inOrderTraversal(TNode root){
+    public void inOrderTraversal(TNode root) {
 
         if (root == null) return;
 
@@ -61,35 +60,113 @@ public class MyTree{
     }
 
 
-
-    //==================================== Depth first ===============================
     // Post-Order Traversal - In Depth
     // Left -> Right -> Root ==> needs to finish all sub-trees before visiting the root
-    public void postOrderTraversal(TNode root){
+    public void postOrderTraversal(TNode root) {
         if (root == null) return;
 
-        postOrderTraversal(root.leftChild);
-        postOrderTraversal(root.rightChild);
-        System.out.println(root.value);
+        postOrderTraversal(root.leftChild);  // branch and finish left subtree
+        postOrderTraversal(root.rightChild); // branch and finish right subtree
+        System.out.println(root.value);      // visit root
 
     }
 
     // ==================================== Breadth First (Level Order) ===========================
     // the logic is visiting the tree level by level starting from the root and from left to right level  by level
 
-    public void levelOrderTraversal(TNode root){
-        if (root == null) return;
+    public void levelOrderTraversal(TNode root) {
+        if (root == null) return;                   // termination
         Queue<TNode> queue = new LinkedList<>();
         queue.add(root);
-        while (!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             TNode toVisit = queue.poll();
+            //if (toVisit.leftChild == null)  System.out.println(toVisit.value); print leaves only
             System.out.println(toVisit.value);
-            if (toVisit.leftChild != null)queue.add(toVisit.leftChild);
-            if (toVisit.rightChild != null)queue.add(toVisit.rightChild);
+            if (toVisit.leftChild != null) queue.add(toVisit.leftChild);
+            if (toVisit.rightChild != null) queue.add(toVisit.rightChild);
         }
 
     }
 
 
+    //TODO: TASKS FROM "Trees_Review Class Notes" PDF
+    // =================== tasks ====================
+
+    // print the leaves of a tree
+    public void printLeaves(TNode root) {
+        if (root == null) return;
+        Queue<TNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TNode toVisit = queue.poll();
+            if (toVisit.leftChild == null && toVisit.rightChild == null) System.out.println(toVisit.value);
+            if (toVisit.leftChild != null) queue.add(toVisit.leftChild);
+            if (toVisit.rightChild != null) queue.add(toVisit.rightChild);
+
+        }
+
+    }
+
+    public void printLeavesRecursiveInOrder(TNode root) {
+        if (root == null) return;
+
+        printLeavesRecursiveInOrder(root.leftChild);
+        if (root.leftChild == null && root.rightChild == null) System.out.println(root.value);
+        printLeavesRecursiveInOrder(root.rightChild);
+    }
+
+    // Task 1: Implement finding an integer value in a BST (Binary Search Tree)
+    public boolean contains(int value) {
+        if (root == null) return false;
+
+        Queue<TNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            TNode toVisit = queue.poll();
+            if (toVisit.value == value) {
+                return true;
+            }
+            if (toVisit.leftChild != null) queue.add(toVisit.leftChild);
+            if (toVisit.rightChild != null) queue.add(toVisit.rightChild);
+        }
+        return false;
+    }
+
+    // Task 2: Implement a method that returns true if the node is a leaf in BST
+    public boolean isLeaf(int value) {
+        if (root == null) return false;
+
+        Queue<TNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TNode toVisit = queue.poll();
+
+            if (value == toVisit.value) {
+                if (toVisit.leftChild == null && toVisit.rightChild == null) {
+                    return true;
+                }
+            }
+            if (toVisit.leftChild != null) queue.add(toVisit.leftChild);
+            if (toVisit.rightChild != null) queue.add(toVisit.rightChild);
+        }
+        return false;
+    }
+
+
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- ds_08_Trees/src/main/java/TreeApp.java
+++ ds_08_Trees/src/main/java/TreeApp.java
@@ -3,10 +3,10 @@ public class TreeApp {
 
         MyTree tree = new MyTree();
 
-        //int[] numbers = {10, 5, 12, 3, 6, 11, 13, 2,};
-        int[] numbers = {10, 6, 8, 20, 4, 9, 5, 17, 42, 47, 29};
+        int[] numbers = {10, 5, 12, 3, 6, 11, 13, 2,};
+        //int[] numbers = {10, 6, 8, 20, 4, 9, 5, 17, 42, 47, 29};
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 8; i++) {
             tree.insert(numbers[i]);
         }
 
@@ -24,6 +24,16 @@ public class TreeApp {
         System.out.println("Level Order");
         tree.levelOrderTraversal(tree.root);
 
+        System.out.println("print leaves");
+        tree.printLeaves(tree.root);
+
+        System.out.println("print leaves recursively");
+        tree.printLeavesRecursiveInOrder(tree.root);
+
+        System.out.println("contains: "+tree.contains(6));
+
+        VisualizeTree.printTree(tree.root, null, false);
+        System.out.println("tree.isLeaf(4) = " + tree.isLeaf(3));
 
 
     }
