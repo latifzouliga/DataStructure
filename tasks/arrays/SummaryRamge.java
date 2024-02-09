78e89fbd7843cb7cffbfb58f60a0a73ec5a7f87e
Abdellatif Zouliga <zuuliga@gmail.com>
2024-02-09 16:11:34 -0500

    SummaryRamge

--- tasks/arrays/SummaryRamge.java
+++ tasks/arrays/SummaryRamge.java
@@ -30,3 +30,17 @@ public class SummaryRamge {
         return list;
     }
 }
+
+/*
+Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of
+nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+Example 1:
+
+Input: nums = [0,1,2,4,5,7]
+Output: ["0->2","4->5","7"]
+Explanation: The ranges are:
+[0,2] --> "0->2"
+[4,5] --> "4->5"
+[7,7] --> "7"
+ */
